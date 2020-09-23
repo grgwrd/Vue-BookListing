78db77fb5e08e1d273300024f09ace0188e763e6
Greg Carlson <grg3@ku.edu>
2020-09-23 16:50:48 -0500

    Add bookform component to book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -6,8 +6,8 @@
       <book-item v-for="book in books" :book="book">
       <!-- Display book items and insert book as props -->
       </book-item>
-      <book-form>
-        
+      <book-form @addBook='appendBook'>
+
       </book-form>
     </ul>
   </div>
@@ -23,17 +23,23 @@
       return {
         title: "All Books",
         books: [
-          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-          { title: 'American Gods', author: 'Neil Gaiman'},
-          { title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+          { title: 'Self-Reliance', author: 'Ralph Waldo Emerson' },
+          { title: 'American Gods', author: 'Neil Gaiman' },
+          { title: 'Amusing Ourselves to Death', author: 'Neil Postman' },
         ],
       }
     },
     components: {
       BookItem,
       BookForm
+    },
+    methods: {
+      appendBook(bookTitle, bookAuthor) {
+        this.books.push({ title: bookTitle, author: bookAuthor });
+      },
     }
 
+  // end export default
   };
 
 </script>
